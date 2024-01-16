badb1a3f4223e3713e71e7c6cd015d59ae591102
Eric Ubaga <ubaga91@gmail.com>
2024-01-16 14:50:26 +0300

    Updated method to wait for 1 second first then yield the random number

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -14,5 +14,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
       (float): yielded random number
     """
     for _ in range(10):
-        yield random.uniform(0, 10)
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
